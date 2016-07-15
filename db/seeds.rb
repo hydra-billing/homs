b66d74426f9732b865f20bd4560218fa07fd5693
Dmitriy Kolesnikov <kolesnikovdmitryi129@gmail.com>
2016-07-15 16:59:04 +0400

    HOMS-111 Add seeds.rb (#17)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,30 @@
-class AddInitialOrderTypes < ActiveRecord::Migration
-  def change
-    [
+def seed
+  add_admin
+  add_initial_order_types
+  add_initial_orders
+end
+
+def add_admin
+  User.connection.schema_cache.clear!
+  User.reset_column_information
+
+  existing_admin = User.find_by(email: 'user@example.com')
+
+  unless existing_admin.present?
+    User.create!(
+        email: 'user@example.com',
+        password: 'changeme',
+        name: 'John',
+        last_name: 'Doe',
+        role: :admin,
+        company: 'Example Corporation',
+        department: 'Administrators',
+        api_token: 'RENEWMEPLEASE')
+  end
+end
+
+def add_initial_order_types
+  [
 "common: &common
   label: ''
   type: string
@@ -112,9 +136,48 @@ order_type:
       <<: *common
       type: number
       label: Pizza Price"].each do |order_type_file|
-      OrderType.create!(
-          file: order_type_file,
-          active: true)
-    end
+    OrderType.create!(
+        file: order_type_file,
+        active: true)
   end
 end
+
+def add_initial_orders
+  # create sequence for order
+  unless Sequence.find_by(name: Sequence.model_sequence_name(Order))
+    OrderSequenceService.new.create
+  end
+
+  # vacation request order
+  order = Order.new(order_type_id: OrderType.find_by(code: 'vacation_request').id)
+
+  order.data = {
+      employeeFirstName: 'James',
+      employeeLastName:  'Carter',
+      employeeEmail:     'james@example.com',
+      beginDate:         (Time.zone.now + 3.months).iso8601,
+      endDate:           (Time.zone.now + 3.months + 1.day).iso8601,
+      motivationText:    'I had a lot of work last 7 years'}
+  order.save
+
+  # support request order
+  order = Order.new(order_type_id: OrderType.find_by(code: 'support_request').id)
+
+  order.data = {
+      requesterName:  'Peter Park',
+      requesterPhone: '12345678900',
+      subject:        'Printer does not work',
+      description:    'I don\'t know what happened but printer in kitchen does not work. Help, please...'}
+  order.save
+
+  # pizza order
+  order = Order.new(order_type_id: OrderType.find_by(code: 'pizza_order').id)
+
+  order.data = {
+      customerName:  'John Smith',
+      customerPhone: '19876543211',
+      pizzaType:     'Margherita'}
+  order.save
+end
+
+seed
