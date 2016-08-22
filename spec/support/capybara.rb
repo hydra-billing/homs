55edb95d57cdf4237803578819c08d0919fc309d
Dmitry Vershinin <vershinin@latera.ru>
2016-08-22 11:36:32 +0300

    Remove duplication

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,5 @@
 Capybara.asset_host = 'http://localhost:3000'
 Capybara.javascript_driver = :poltergeist
-Capybara.javascript_driver = :poltergeist
 Capybara.automatic_reload = false
 Capybara::Screenshot.register_filename_prefix_formatter(:rspec) do |example|
   "screenshot_#{example.full_description.gsub(' ', '-').gsub(/^.*\/spec\//,'')}"
