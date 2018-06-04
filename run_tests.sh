14c1b78ba7280e2af427ee6b5a4bac439e826a3d
TimAle <t.alekperov@latera.ru>
2018-06-04 13:37:14 +0300

    HOMS-195 Run tests in docker

---
+++ run_tests.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+RAILS_ENV=test
+
+/wait_for_postgres.sh db_homs
+
+# prepare configs
+cp config/activiti.yml.sample config/activiti.yml
+cp config/database.yml.sample config/database.yml
+cp config/secrets.yml.sample config/secrets.yml
+echo "sources:
+  bpmanagementsystem:
+    type: 'static/activiti'
+" > config/sources.yml
+
+bundle exec rake db:migrate && bundle exec rake db:seed
+
+# add source billing for tests
+echo "sources:
+  bpmanagementsystem:
+    type: 'static/activiti'
+  billing:
+    type: sql/oracle
+    tns_name: test
+    username: test
+    password: test
+" > config/sources.yml
+
+bundle exec rspec ./spec --format RspecJunitFormatter --out test-reports/out.xml --format progress
+
+if [ "$GENERATE_DOC" = 'true' ]; then bundle exec rake docs:generate && zip -r doc.zip public/doc; fi
