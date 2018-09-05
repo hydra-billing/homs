4926a842dd0f30244d06d928d47ca8f8ee1c540d
Dmitry Vershinin <sugarfree1@users.noreply.github.com>
2018-09-05 15:56:16 +0300

    HBW-61 Add support for Camunda (#149)

--- hbw/app/models/hbw/bpm_user.rb
+++ hbw/app/models/hbw/bpm_user.rb
@@ -5,14 +5,26 @@ module HBW
 
     class << self
       def fetch(email)
-        definition = do_request(:get, 'identity/users', email: email).first
+        definition = do_request(:get, users_url, email: email).first
         new(definition) if definition
       end
 
       def fetch_all
-        do_request(:get, 'identity/users', firstNameLike: '%').map do |definition|
+        do_request(:get, users_url, firstNameLike: '%').map do |definition|
           new(definition) if definition
         end.compact
+      end
+
+      def activiti?
+        HBW::Widget.config.fetch(:adapter) == 'activiti'
+      end
+
+      def users_url
+        if activiti?
+          'identity/users'
+        else
+          'user'
+        end
       end
     end
 
--- hbw/app/models/hbw/deployment.rb
+++ hbw/app/models/hbw/deployment.rb
@@ -2,14 +2,27 @@ module HBW
   class Deployment
     extend HBW::Remote
     include HBW::Definition
+    include HBW::Inject[:config]
 
     class << self
       def fetch(process_definition)
         deployment_id = process_definition.deployment_id
-        resources = do_request(:get, 'repository/deployments/%s/resources' % deployment_id)
-        new(do_request(:get, 'repository/deployments/%s' % deployment_id).merge(
+        resources = do_request(:get, "#{deployments_url_prefix}/%s/resources" % deployment_id)
+        new(do_request(:get, "#{deployments_url_prefix}/%s" % deployment_id).merge(
           'resources' => resources
         ))
+      end
+
+      def activiti?
+        HBW::Widget.config.fetch(:adapter) == 'activiti'
+      end
+
+      def deployments_url_prefix
+        if activiti?
+          'repository/deployments'
+        else
+          '/rest/deployment'
+        end
       end
     end
 
@@ -17,7 +30,15 @@ module HBW
 
     def resource(resource_id)
       resources.find do |resource|
-        resource['id'] == resource_id
+        resource[resource_key] == resource_id
+      end
+    end
+
+    def resource_key
+      if config.fetch(:adapter) == 'activiti'
+        'id'
+      else
+        'name'
       end
     end
   end
