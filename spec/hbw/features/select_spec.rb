a13615f47f60d57d682c30baa393c214d6d50ac0
TimAle <t.alekperov@latera.ru>
2019-06-11 17:09:32 +0300

    HBW-150 Test select & select_table with different options combinations

--- spec/hbw/features/select_spec.rb
+++ spec/hbw/features/select_spec.rb
@@ -1,4 +1,4 @@
-feature 'Check select with', js: true do
+feature 'Check select', js: true do
   let(:placeholder) { 'placeholder' }
   let(:first_value) { 'Option 1' }
 
@@ -22,131 +22,158 @@ feature 'Check select with', js: true do
     FactoryBot.create(:order, order_type: order_type)
     FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-12')
     FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-17')
+    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-22')
   end
 
-  scenario 'nullable = true and placeholder present' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
+  describe 'rendering with options' do
+    before(:each) do
+      click_on 'Orders'
+      expect(page).to have_content 'Orders list'
+      expect_widget_presence
+    end
 
-    click_and_wait 'ORD-3'
+    scenario 'nullable = true and placeholder present' do
+      click_and_wait 'ORD-3'
 
-    expect(page).to have_content 'ORD-3'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
-    expect_widget_presence
+      expect(page).to have_content 'ORD-3'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
+      expect_widget_presence
+    end
+
+    scenario 'nullable = false and placeholder is provided but not shown' do
+      click_and_wait 'ORD-4'
+
+      expect(page).to have_content 'ORD-4'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
+      expect_widget_presence
+    end
+
+    scenario 'nullable = true and placeholder blank' do
+      click_and_wait 'ORD-5'
+
+      expect(page).to have_content 'ORD-5'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_placeholder('homsOrderDataSelect', visible: false)).to eq ''
+      expect_widget_presence
+    end
+
+    scenario 'nullable = false and placeholder blank' do
+      click_and_wait 'ORD-6'
+
+      expect(page).to have_content 'ORD-6'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
+      expect_widget_presence
+    end
+
+    scenario 'defining choices as arrays' do
+      click_and_wait 'ORD-7'
+
+      expect(page).to have_content 'ORD-7'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 1', value: '112233'})
+
+      set_r_select_option('homsOrderDataSelect', 'Option 2')
+
+      expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 2', value: '445566'})
+      expect_widget_presence
+    end
+
+    scenario 'defining choices as strings' do
+      click_and_wait 'ORD-8'
+
+      expect(page).to have_content 'ORD-8'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 1', value: 'Option 1'})
+
+      set_r_select_option('homsOrderDataSelect', 'Option 2')
+
+      expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 2', value: 'Option 2'})
+      expect_widget_presence
+    end
+
+    scenario 'field name not defined in BP variables' do
+      click_and_wait 'ORD-9'
+
+      expect(page).to have_content 'ORD-9'
+      expect_r_select_presence('homsOrderNotInVBPVariables')
+      expect(page).to have_content 'Field with name homsOrderNotInVBPVariables not defined in BP variables'
+      expect_widget_presence
+    end
+
+    scenario 'BP variable value is null for required field' do
+      click_and_wait 'ORD-12'
+
+      expect(page).to have_content 'ORD-12'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
+      expect(page).to have_no_content 'Field with name homsOrderNotInVBPVariables not defined in BP variables'
+      expect_widget_presence
+    end
   end
 
-  scenario 'nullable = false and placeholder present' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
+  describe 'will be submitted with lookup mode' do
+    before(:each) do
+      click_on 'Orders'
+      expect(page).to have_content 'Orders list'
+      expect_widget_presence
+    end
 
-    click_and_wait 'ORD-4'
+    scenario 'nullable = true, value is empty' do
+      click_and_wait 'ORD-17'
 
-    expect(page).to have_content 'ORD-4'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
-    expect_widget_presence
+      expect(page).to have_content 'ORD-17'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field is required'
+      expect_widget_presence
+    end
+
+    scenario 'nullable = false, value is set' do
+      click_and_wait 'ORD-22'
+
+      expect(page).to have_content 'ORD-22'
+      expect_r_select_presence('homsOrderDataSelect')
+      expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
+
+      r_select_input('homsOrderDataSelect').set('text')
+      wait_for_ajax
+
+      expect(r_select_lookup_options('homsOrderDataSelect')).to eq ['Some text']
+
+      set_r_select_lookup_option('homsOrderDataSelect', 'Some text')
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field is required'
+      expect_widget_presence
+    end
   end
 
-  scenario 'nullable = true and placeholder blank' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
+  describe 'will not be submitted with lookup mode' do
+    before(:each) do
+      click_on 'Orders'
+      expect(page).to have_content 'Orders list'
+      expect_widget_presence
+    end
 
-    click_and_wait 'ORD-5'
+    scenario 'nullable = false, value is empty' do
+      click_and_wait 'ORD-22'
 
-    expect(page).to have_content 'ORD-5'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_placeholder('homsOrderDataSelect', visible: false)).to eq ''
-    expect_widget_presence
-  end
+      expect(page).to have_content 'ORD-22'
+      expect_r_select_presence('homsOrderDataSelect')
+      ensure_r_select_is_empty('homsOrderDataSelect')
+      expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
 
-  scenario 'nullable = false and placeholder blank' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
+      click_and_wait 'Submit'
 
-    click_and_wait 'ORD-6'
-
-    expect(page).to have_content 'ORD-6'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
-    expect_widget_presence
-  end
-
-  scenario 'defining choices as arrays' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
-
-    click_and_wait 'ORD-7'
-
-    expect(page).to have_content 'ORD-7'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 1', value: '112233'})
-
-    set_r_select_option('homsOrderDataSelect', 'Option 2')
-
-    expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 2', value: '445566'})
-    expect_widget_presence
-  end
-
-  scenario 'defining choices as strings' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
-
-    click_and_wait 'ORD-8'
-
-    expect(page).to have_content 'ORD-8'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 1', value: 'Option 1'})
-
-    set_r_select_option('homsOrderDataSelect', 'Option 2')
-
-    expect(r_select_single_value('homsOrderDataSelect')).to eq({label: 'Option 2', value: 'Option 2'})
-    expect_widget_presence
-  end
-
-  scenario 'field name not defined in BP variables' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
-
-    click_and_wait 'ORD-9'
-
-    expect(page).to have_content 'ORD-9'
-    expect_r_select_presence('homsOrderNotInVBPVariables')
-    expect(page).to have_content 'Field with name homsOrderNotInVBPVariables not defined in BP variables'
-    expect_widget_presence
-  end
-
-  scenario 'BP variable value is null for required field' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
-
-    click_and_wait ('ORD-12')
-
-    expect(page).to have_content 'ORD-12'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_single_value('homsOrderDataSelect')[:label]).to eq first_value
-    expect(page).to have_no_content 'Field with name homsOrderNotInVBPVariables not defined in BP variables'
-    expect_widget_presence
-  end
-
-  scenario 'nullable = true, placeholder present and mode = lookup' do
-    click_on 'Orders'
-    expect(page).to have_content 'Orders list'
-    expect_widget_presence
-
-    click_and_wait 'ORD-17'
-
-    expect(page).to have_content 'ORD-17'
-    expect_r_select_presence('homsOrderDataSelect')
-    expect(r_select_placeholder('homsOrderDataSelect')).to eq placeholder
-    expect_widget_presence
+      expect(page).to have_content 'Field is required'
+      expect_widget_presence
+    end
   end
 end
--- spec/hbw/features/select_table_spec.rb
+++ spec/hbw/features/select_table_spec.rb
@@ -1,4 +1,4 @@
-feature 'Check select table with', js: true do
+feature 'Select table', js: true do
   before(:each) do
     user = FactoryBot.create(:user)
 
@@ -9,9 +9,13 @@ feature 'Check select table with', js: true do
     order_type = FactoryBot.create(:order_type, :support_request)
 
     FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-14')
+    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-18')
+    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-19')
+    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-20')
+    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-21')
   end
 
-  scenario 'shows table with static variants' do
+  scenario 'rendered with static variants' do
     click_on 'Orders'
     expect(page).to have_content 'Orders list'
     expect_widget_presence
@@ -33,5 +37,102 @@ feature 'Check select table with', js: true do
     expect(page).to have_content '3,252,349,284.00'
 
     expect_widget_presence
+  end
+
+  describe 'will be submitted if' do
+    before(:each) do
+      click_on 'Orders'
+      expect(page).to have_content 'Orders list'
+      expect_widget_presence
+    end
+
+    scenario 'empty nullable multi = false' do
+      click_and_wait 'ORD-18'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field does not match regex'
+      expect(page).not_to have_content 'Field is required'
+
+      expect_widget_presence
+    end
+
+    scenario 'empty nullable multi = true' do
+      click_and_wait 'ORD-19'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field does not match regex'
+      expect(page).not_to have_content 'Field is required'
+
+      expect_widget_presence
+    end
+
+    scenario 'filled required multi = false' do
+      click_and_wait 'ORD-20'
+
+      set_select_table_option 'Some name'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field does not match regex'
+      expect(page).not_to have_content 'Field is required'
+
+      expect_widget_presence
+    end
+
+    scenario 'filled required multi = true' do
+      click_and_wait 'ORD-21'
+
+      set_select_table_option 'Some name'
+      set_select_table_option 'Other name'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).not_to have_content 'Field does not match regex'
+      expect(page).not_to have_content 'Field is required'
+
+      expect_widget_presence
+    end
+  end
+
+  describe 'will not be submitted if empty required' do
+    before(:each) do
+      click_on 'Orders'
+      expect(page).to have_content 'Orders list'
+      expect_widget_presence
+    end
+
+    scenario 'multi = false' do
+      click_and_wait 'ORD-20'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).to have_content 'Field is required'
+
+      expect_widget_presence
+    end
+
+    scenario 'multi = true' do
+      click_and_wait 'ORD-21'
+
+      expect(page).to have_selector "button[type='submit']"
+
+      click_and_wait 'Submit'
+
+      expect(page).to have_content 'Field is required'
+
+      expect_widget_presence
+    end
   end
 end
