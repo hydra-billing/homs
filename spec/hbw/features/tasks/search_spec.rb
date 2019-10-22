40ca3a939784b51a134cae925e8e08413aedc892
Maxim Smirnov <m.smirnov@latera.ru>
2019-10-22 18:21:53 +0300

    HBW-191 Add tests for searching in claiming table

---
+++ spec/hbw/features/tasks/search_spec.rb
@@ -0,0 +1,70 @@
+feature 'Check searching in table with tasks', js: true do
+  before(:each) do
+    user = FactoryBot.create(:user)
+
+    signin(user.email, user.password)
+    expect(page).not_to have_content 'Sign in'
+    expect(page).to     have_content 'Orders list'
+
+    click_and_wait 'Tasks'
+    expect_widget_presence
+    expect(page).to have_content 'Open tasks'
+  end
+
+  describe 'search field rendered' do
+    it 'with too short search query' do
+      expect(tasks_table_content).to eq(
+        [
+          ['Medium', 'Check test form', ' Test name', 'No description', 'expired'],
+          ['High', 'Check test form', ' Test name', 'Some test description', '30 Jun 2016']
+        ]
+      )
+
+      fill_search_field('te')
+
+      expect(tasks_table_content).to eq(
+        [
+          ['Medium', 'Check test form', ' Test name', 'No description', 'expired'],
+          ['High', 'Check test form', ' Test name', 'Some test description', '30 Jun 2016']
+        ]
+      )
+    end
+
+    it 'with valid search request and clearing on cross click' do
+      expect(tasks_table_content).to eq(
+        [
+          ['Medium', 'Check test form', ' Test name', 'No description', 'expired'],
+          ['High', 'Check test form', ' Test name', 'Some test description', '30 Jun 2016']
+        ]
+      )
+
+      fill_search_field('test')
+
+      expect(search_field_text).to eq('test')
+      expect(tasks_table_content).to eq(
+        [
+          ['High', 'Check test form', ' Test name', 'Some test description', '30 Jun 2016']
+        ]
+      )
+
+      clear_search_field
+
+      expect(search_field_text).to eq('')
+      expect(tasks_table_content).to eq(
+        [
+          ['Medium', 'Check test form', ' Test name', 'No description', 'expired'],
+          ['High', 'Check test form', ' Test name', 'Some test description', '30 Jun 2016']
+        ]
+      )
+    end
+
+    it 'with clearing after tab changes' do
+      fill_search_field('test')
+      expect(search_field_text).to eq('test')
+
+      click_on_tab 'Unassigned tasks (10)'
+
+      expect(search_field_text).to eq('')
+    end
+  end
+end
