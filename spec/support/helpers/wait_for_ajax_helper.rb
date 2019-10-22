66de4ddf9d8b8de5128877a1497cce89c2faa06e
Maxim Smirnov <m.smirnov@latera.ru>
2019-10-22 18:21:53 +0300

    HBW-191 Add tests for searching in claiming table

--- spec/support/helpers/wait_for_ajax_helper.rb
+++ spec/support/helpers/wait_for_ajax_helper.rb
@@ -10,5 +10,17 @@ module Features
       result = page.evaluate_script('jQuery.active')
       result.nil? || result.zero?
     end
+
+    def ajax
+      tick
+      yield
+      tick
+    ensure
+      wait_for_ajax
+    end
+
+    def tick
+      sleep(Capybara.default_max_wait_time / 2)
+    end
   end
 end
