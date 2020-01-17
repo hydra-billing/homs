6daeb110c186320d7f7a34006d538e588ab33aa1
GideonEXE <kolesnikovdmitryi129@gmail.com>
2020-01-17 12:56:46 +0300

    HBW-258 Move action cable to hbw

--- hbw/config/initializers/hbw.rb
+++ hbw/config/initializers/hbw.rb
@@ -7,4 +7,6 @@ Rails.application.config.to_prepare do
   validated_config = HBW::ConfigValidator.(Settings::HBW.deep_symbolize_keys)
 
   HBW::Widget.config = HBW::Config.new(validated_config.to_h)[:hbw]
+
+  Rails.application.config.action_cable.mount_path = '/widget/cable'
 end
