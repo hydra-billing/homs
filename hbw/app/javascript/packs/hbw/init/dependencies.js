a0fb507464e757829d22018732bd20312187dd41
Dmitry Vershinin <vershinin@latera.ru>
2020-01-17 10:12:14 +0300

    HBW-261 Remove select2 dependency from widget

--- hbw/app/javascript/packs/hbw/init/dependencies.js
+++ hbw/app/javascript/packs/hbw/init/dependencies.js
@@ -2,7 +2,5 @@ import './noconflict';
 import 'jquery';
 import 'moment';
 import 'bootstrap-datetimepicker';
-import 'select2/select2.full';
-import 'select2/i18n/ru';
 import 'tooltip';
 import './wrap_globals';
