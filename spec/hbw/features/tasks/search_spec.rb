4630c7f5260a502bb3b933343a457a5efaf87484
vladdoncheg <vladdoncheg@gmail.com>
2020-10-28 13:53:27 +0000

    HBW-143 Test dynamic conditions dependent on select

--- spec/hbw/features/tasks/search_spec.rb
+++ spec/hbw/features/tasks/search_spec.rb
@@ -6,7 +6,7 @@ feature 'Check searching in table with tasks', js: true do
     [
       ['Medium', 'Assigned task', ' Test name', '—', "expired (#{years_since(first_task_due_date)}y past due date)"],
       ['High', 'Other assigned task', ' Test name', 'Some test description', "expired (#{years_since(second_task_due_date)}y past due date)"],
-      *Array.new(27) { ['High', 'Check test form', ' Test name', '—', '30 Jun 2016'] }
+      *Array.new(29) { ['High', 'Check test form', ' Test name', '—', '30 Jun 2016'] }
     ]
   end
 
