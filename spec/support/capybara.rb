deef0219454b1f4d96242c120f0c673a6fa069ba
TimAle <t.alekperov@latera.ru>
2020-12-17 13:01:33 +0300

    HOMS-288 Fix tests with fontawesome icons

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,6 @@
 Capybara.register_driver :chrome do |app|
+  Selenium::WebDriver::Chrome::Service.driver_path = '/usr/local/bin/chromedriver'
+
   opts = Selenium::WebDriver::Chrome::Options.new
 
   chrome_args = %w[--headless --window-size=1920,1080 --no-sandbox --disable-dev-shm-usage]
