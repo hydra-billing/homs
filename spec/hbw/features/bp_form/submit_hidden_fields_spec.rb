7dd1e031153197325d951c7227aa118b0dc81bdc
Dmitrii Sergeichuk <sergeychukdmitry@gmail.com>
2021-08-12 17:48:02 +0300

    HOMS-373 Refactor test submit_hidden_fields_spec

--- spec/hbw/features/bp_form/submit_hidden_fields_spec.rb
+++ spec/hbw/features/bp_form/submit_hidden_fields_spec.rb
@@ -1,14 +1,16 @@
 feature 'Submit form', js: true do
   before(:each) do
+    set_camunda_api_mock_file('spec/hbw/features/bp_form/submit_hidden_fields_mock.yml')
+
     order_type = FactoryBot.create(:order_type, :support_request)
-    FactoryBot.create(:order, order_type: order_type).update(code: 'ORD-29')
+    FactoryBot.create(:order, order_type: order_type)
     user = FactoryBot.create(:user)
 
     signin(user.email, user.password)
     expect(page).not_to have_content 'Sign in'
     expect(page).to     have_content 'Orders list'
 
-    click_and_wait 'ORD-29'
+    click_and_wait 'ORD-1'
 
     expect(page).to have_selector "[name='submittedString']"
   end
