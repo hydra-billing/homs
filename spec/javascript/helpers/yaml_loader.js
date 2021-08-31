0e31b95479d38f387fbc393112de30f83c757b42
timale <t.alekperov@latera.ru>
2021-08-31 11:42:41 +0300

    HOMS-372 Fix updated js-yaml usage

--- spec/javascript/helpers/yaml_loader.js
+++ spec/javascript/helpers/yaml_loader.js
@@ -2,7 +2,7 @@ const yaml = require('js-yaml');
 
 module.exports = {
   process (source) {
-    const res = yaml.safeLoad(source);
+    const res = yaml.load(source);
     return `module.exports = ${JSON.stringify(res, undefined, '\t')};`;
   },
 };
