7ddc22df9ffc874b8dba36ed58cd6d4cd0e97a71
timale <t.alekperov@latera.ru>
2023-03-23 17:55:21 +0300

    HOMS-454 Update i18n-js

--- app/javascript/init/translations.js
+++ app/javascript/init/translations.js
@@ -1,7 +1,11 @@
-import 'i18n-js';
+import { I18n } from 'i18n-js';
 
 import en from '../../../config/locales/en.yml';
 import ru from '../../../config/locales/ru.yml';
 import es from '../../../config/locales/es.yml';
 
-I18n.translations = { ...en, ...ru, ...es };
+const i18n = new I18n();
+
+i18n.store({ ...en, ...ru, ...es });
+
+window.I18n = i18n;
