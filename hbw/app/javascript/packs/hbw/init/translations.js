cc29daeac45469f6a590bdd3757cf4e9d2c10186
timale <t.alekperov@latera.ru>
2023-03-23 16:35:23 +0300

    HOMS-454 Update yaml-loader

--- hbw/app/javascript/packs/hbw/init/translations.js
+++ hbw/app/javascript/packs/hbw/init/translations.js
@@ -1,4 +1,4 @@
-import { en } from '../../../../../config/locales/en.yml';
-import { ru } from '../../../../../config/locales/ru.yml';
+import en from '../../../../../config/locales/en.yml';
+import ru from '../../../../../config/locales/ru.yml';
 
-export default { en, ru };
+export default { ...en, ...ru };
