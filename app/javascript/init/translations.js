7baa5ac41c1d80ca1e49e28d64f8145e018a6209
timale <t.alekperov@latera.ru>
2023-03-23 16:35:23 +0300

    HOMS-454 Update yaml-loader

--- app/javascript/init/translations.js
+++ app/javascript/init/translations.js
@@ -1,6 +1,7 @@
 import 'i18n-js';
-import { en } from '../../../config/locales/en.yml';
-import { ru } from '../../../config/locales/ru.yml';
-import { es } from '../../../config/locales/es.yml';
 
-I18n.translations = { en, ru, es };
+import en from '../../../config/locales/en.yml';
+import ru from '../../../config/locales/ru.yml';
+import es from '../../../config/locales/es.yml';
+
+I18n.translations = { ...en, ...ru, ...es };
