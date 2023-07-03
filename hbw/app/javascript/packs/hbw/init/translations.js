b0c765443eae4d0dbae396d106b7b3dbaeeade44
Roman Kulemin <r.kulemin@latera.ru>
2023-07-03 14:44:51 +0300

    CONSULT-6505 Add es locale to widget

--- hbw/app/javascript/packs/hbw/init/translations.js
+++ hbw/app/javascript/packs/hbw/init/translations.js
@@ -1,4 +1,5 @@
 import en from '../../../../../config/locales/en.yml';
 import ru from '../../../../../config/locales/ru.yml';
+import es from '../../../../../config/locales/es.yml';
 
-export default { ...en, ...ru };
+export default { ...en, ...ru, ...es };
