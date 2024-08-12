4769a75b09042bc77b0b3886fa7d91576f9b915c
timale <t.alekperov@latera.ru>
2024-08-12 13:14:05 +0300

    HOMS-491 Support `fr` locale

--- app/javascript/init/translations.js
+++ app/javascript/init/translations.js
@@ -3,9 +3,12 @@ import { I18n } from 'i18n-js';
 import en from '../../../config/locales/en.yml';
 import ru from '../../../config/locales/ru.yml';
 import es from '../../../config/locales/es.yml';
+import fr from '../../../config/locales/fr.yml';
 
 const i18n = new I18n();
 
-i18n.store({ ...en, ...ru, ...es });
+i18n.store({
+  ...en, ...ru, ...es, ...fr
+});
 
 window.I18n = i18n;
