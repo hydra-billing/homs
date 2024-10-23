34e3fd2c59aa91ff0ea40392760fedb59cfa5f3d
Igor Nikolaev <igor.nikolaev1990@gmail.com>
2024-10-23 22:43:39 +0300

    HBW-416 Translate descriptions

--- hbw/app/javascript/packs/hbw/components/form/datetime.js.jsx
+++ hbw/app/javascript/packs/hbw/components/form/datetime.js.jsx
@@ -76,9 +76,13 @@ modulejs.define('HBWFormDatetime', ['React'], (React) => {
     }
 
     renderDescription = () => {
-      const { placement, text } = this.props.params.description;
+      const { name, params, task } = this.props;
 
-      return <div className="description" data-test={`description-${placement}`}>{text}</div>;
+      const { placement, text } = params.description;
+
+      const translated = this.context.translateBP(`${task.process_key}.${task.key}.${name}.description`, {}, text);
+
+      return <div className="description" data-test={`description-${placement}`}>{translated}</div>;
     };
 
     componentWillUnmount () {
